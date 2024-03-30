d4afc9436230aa0b8243a18e1975aa968dfeaeea
vincevise <88813613+vincevise@users.noreply.github.com>
2024-03-30 22:34:36 +0530

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,7 @@ import { authMiddleware,withClerkMiddleware } from "@clerk/nextjs";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-    publicRoutes: ["/", "/contact","/loading"]  
+    publicRoutes: ["/", "/contact","/loading", "/api/trpc/note.getNotes"]  
 });
  
 export const config = {
